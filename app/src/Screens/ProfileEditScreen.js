8053b2b73ae858dd61341669e62f69fe80d07a78
Tim Lane <timlanesoftware@gmail.com>
2022-02-19 13:14:32 +0000

    Added date picker modal (wip)

--- app/src/Screens/ProfileEditScreen.js
+++ app/src/Screens/ProfileEditScreen.js
@@ -1,51 +1,75 @@
-import React, { useState } from 'react';
-import { Dimensions, Image, StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { Component, createRef, useState } from 'react';
+import { Button, Dimensions, Image, StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
 import Text from '../Components/Text';
 import StyleMain from '../Stylesheets/StyleMain';
-import AvatarIcon from "../../assets/icons/account.svg"
-import DatePicker from "react-native-date-picker"
+import AvatarIcon from "../../assets/icons/account.svg";
+import DatePicker from 'react-native-modern-datepicker';
+import Modal from 'react-native-modalbox';
+import LogBoxInspectorContainer from 'react-native/Libraries/LogBox/LogBoxInspectorContainer';
 
 const dimensions = Dimensions.get('window');
 const avatarSize = dimensions.width * 0.6;
 
-function ProfileEditScreen({ navigation }) {
-    const [date, setDate] = useState(new Date())
-    const [open, setOpen] = useState(false)
+/*
 
-    return (
-        <View style={[StyleMain.background]}>
-            <View style={[StyleMain.topContent, StyleMain.center]}>
-                <View style={[StyleMain.avatar, StyleMain.centerContent, styles.avatar, {marginTop: 10}]}>
-                    <Image source={require("../../assets/favicon.png")} style={styles.avatar} />
-                    <AvatarIcon width={avatarSize} height={avatarSize} style={{position: "absolute"}} />
+*/
+
+export default class ProfileEditScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.datePickerRef = React.createRef();
+        this.state = {
+            selectedDate: "",
+            isOpen: false,
+        };
+    }
+
+    render() {
+        return (
+            <View style={[StyleMain.background]}>
+
+                <Modal
+                    style={[styles.modal]}
+                    position={"center"}
+                    ref={this.datePickerRef}
+                >
+                    <DatePicker
+                        onSelectedChange={date => {
+                            if (this.state.selectedDate != date) {
+                                this.setState({ selectedDate: date });
+                            }
+                        }}
+                        mode="calendar"
+                    />
+
+                    <Button title="Set Date" onPress={() => { 
+                        this.datePickerRef.current.close();
+                        this.setState({isOpen: false});
+                    }} />
+                </Modal>
+
+                <View style={[StyleMain.topContent, StyleMain.center]}>
+                    <View style={[StyleMain.avatar, StyleMain.centerContent, styles.avatar, {marginTop: 10}]}>
+                        <Image source={require("../../assets/favicon.png")} style={styles.avatar} />
+                        <AvatarIcon width={avatarSize} height={avatarSize} style={{position: "absolute"}} />
+                    </View>
                 </View>
+
+                <View style={StyleMain.mainContent}>
+                    
+                    <Text style={styles.text}>Name:</Text>
+                    <TextInput style={StyleMain.textInput}></TextInput>
+
+                    <Text style={styles.text}>Date of birth:</Text>
+                    <TouchableOpacity style={StyleMain.textInput} onPress={() => this.datePickerRef.current.open()}>
+                        <Text>{new Date(Date.parse(this.state.selectedDate)).toLocaleDateString()}</Text>
+                    </TouchableOpacity>
+
+                </View>
+
             </View>
-
-            <View style={StyleMain.mainContent}>
-                
-                <Text style={styles.text}>Name:</Text>
-                <TextInput style={StyleMain.textInput}></TextInput>
-
-                <Text style={styles.text}>Date of birth:</Text>
-                <TouchableOpacity style={StyleMain.textInput} onPress={() => setOpen(true)}>
-                    <Text>{date.toLocaleDateString()}</Text>
-                </TouchableOpacity>
-                <DatePicker
-                    modal
-                    open={open}
-                    date={date}
-                    onConfirm={(date) => {
-                        setOpen(false)
-                        setDate(date)
-                    }}
-                        onCancel={() => {
-                        setOpen(false)
-                    }}
-                />
-            </View>
-
-        </View>
-    );
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -54,10 +78,12 @@ const styles = StyleSheet.create({
         height: avatarSize,
         borderRadius: 10000
     },
+    modal: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     text: {
         marginTop: 12,
         marginBottom: 4
     }
 })
-
-export default ProfileEditScreen;
