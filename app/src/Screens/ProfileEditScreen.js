b16268125ae6736cd144810b9af471596a4b1dd6
Tim Lane <timlanesoftware@gmail.com>
2022-02-21 16:46:27 +0000

    Setup profile image cropping

--- app/src/Screens/ProfileEditScreen.js
+++ app/src/Screens/ProfileEditScreen.js
@@ -23,7 +23,8 @@ export default class ProfileEditScreen extends React.Component {
             name: "",
             location: "",
             website: "",
-            bio: ""
+            bio: "",
+            avatar: "", // Base-64 encoded string
         };
     }
 
@@ -34,6 +35,7 @@ export default class ProfileEditScreen extends React.Component {
     onOpen() {
         if (typeof this.state !== 'undefined')
         {
+            ImagePicker.clean();
             console.log("Opened profile edit screen");
             if (Database.active != null) {
                 // Load active entity data
@@ -42,6 +44,7 @@ export default class ProfileEditScreen extends React.Component {
                 this.setState({ location: Database.active.getString("profile.location") });
                 this.setState({ website: Database.active.getString("profile.website") });
                 this.setState({ bio: Database.active.getString("profile.bio") });
+                this.setState({ avatar: Database.active.getString("profile.avatar") });
             } else {
                 // No entity is loaded, must be part of entity creation flow.
                 this.setState({ name: "" });
@@ -49,6 +52,7 @@ export default class ProfileEditScreen extends React.Component {
                 this.setState({ location: "" });
                 this.setState({ website: "" });
                 this.setState({ bio: "" });
+                this.setState({ avatar: "" });
             }
         }
     }
@@ -79,14 +83,33 @@ export default class ProfileEditScreen extends React.Component {
                                 style={[StyleMain.avatar, styles.avatar, {marginTop: 10}]}
                                 onPress={() => {
                                     ImagePicker.openPicker({
-                                        cropping: true
+                                        width: 400,
+                                        height: 400,
+                                        cropping: true,
+                                        writeTempFile: false,
+                                        includeBase64: true,
+                                        avoidEmptySpaceAroundImage: true,
+                                        cropperCircleOverlay: true
                                     }).then(image => {
-                                        console.log(image);
+                                        this.setState({avatar: JSON.stringify(image)});
+                                    }, () => {
+                                        // Do nothing if cancelled
                                     });
                                 }}
                             >
-                                <Image source={require("../../assets/favicon.png")} style={styles.avatar} />
-                                <AvatarIcon width={avatarSize} height={avatarSize} style={{position: "absolute"}} />
+                                {
+                                // Load avatar from database if available
+                                (() => {
+                                    if (this.state.avatar != "") {
+                                        return (
+                                        <Image
+                                            source={{uri: "data:" + JSON.parse(this.state.avatar).mime + ";base64," + JSON.parse(this.state.avatar).data}}
+                                            style={styles.avatar}
+                                        />);
+                                    }
+                                    return <AvatarIcon width={avatarSize} height={avatarSize} style={{position: "absolute"}} />
+                                })()
+                                }
                             </TouchableOpacity>
                         </View>
                         <Text style={styles.text}>Name:</Text>
@@ -148,6 +171,7 @@ export default class ProfileEditScreen extends React.Component {
                             Database.active.set("profile.location", this.state.location);
                             Database.active.set("profile.website", this.state.website);
                             Database.active.set("profile.bio", this.state.bio);
+                            Database.active.set("profile.avatar", this.state.avatar);
                         }}
                     >
                         <ButtonText style={{color: (this.state.name != "" ? Colors.buttonText : Colors.buttonTextDisabled)}}>Save Profile</ButtonText>
