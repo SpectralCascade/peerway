db1145af622df2b67e27db2faa963e7cf26ee6e3
Tim Lane <timlanesoftware@gmail.com>
2022-03-19 17:36:18 +0000

    Message loading now allows for loading old messages

--- Peerway/app/src/Screens/Chat.js
+++ Peerway/app/src/Screens/Chat.js
@@ -33,7 +33,10 @@ export default class Chat extends React.Component {
                     name: 'React Native',
                     avatar: 'https://placeimg.com/140/140/any',
                 }
-            }*/]
+            }*/],
+            block: 0,
+            loadedTS: 0,
+            began: 0
         };
 
         // Get the chat ID
@@ -52,7 +55,7 @@ export default class Chat extends React.Component {
             timestamp,
             index
         ).then((block) => {
-            Log.Debug("Opened chat data file, loaded block: " + JSON.stringify(block));
+            //Log.Debug("Opened chat data file, loaded block: " + JSON.stringify(block));
             for (let i = block.length - 1; i >= 0; i--) {
                 let item = block[i];
                 this.state.messages.push({
@@ -73,37 +76,71 @@ export default class Chat extends React.Component {
         });
     }
 
-    // Called when the screen is opened.
-    onOpen() {
-        this.chatId = this.props.route.params.chatId;
-        console.log("OPENED CHAT with id: " + this.chatId);
+    LoadMessages() {
+        Log.Debug("Attempting to load more messages...");
 
-        // Find the most recent messages
-        this.state.messages = [];
-        let date = new Date(Date.now());
-        let millis = date.valueOf();
+        // Must have just opened the chat, need to initialise to most recent time
+        if (this.state.loadedTS == 0) {
+            Log.Debug("Initialising chat messages");
+            // Set initial timestamp to next month, so the first loop does nothing
+            let nextMonth = new Date(Date.now());
+            nextMonth.setUTCMonth(nextMonth.getUTCMonth() + 1);
+            this.state.loadedTS = nextMonth.valueOf();
+            this.state.block = -1;
+        }
+
+        let date = new Date(this.state.loadedTS);
+        this.state.loadedTS = date.valueOf();
         let meta = {};
 
         // Check if there are actually any messages
         if (Database.active.contains(this.GetChatDataPath())) {
-            let began = JSON.parse(Database.active.getString(this.GetChatDataPath())).began;
-            while (!("blocks" in meta) && began < millis) {
-                millis = date.valueOf();
-                meta = Database.GetStoreMeta(Database.active, this.GetChatDataPath(), millis);
-                if ("blocks" in meta) {
-                    // Show most recent messages
-                    this.LoadMessageBlock(millis, meta.blocks.length - 1).then(() => { this.forceUpdate(); });
+            while (!("blocks" in meta) && this.state.began < this.state.loadedTS) {
+                meta = Database.GetStoreMeta(Database.active, this.GetChatDataPath(), this.state.loadedTS);
+                let hasBlocks = "blocks" in meta;
+                if (this.state.block < 0) {
+                    // Start from end block
+                    this.state.block = hasBlocks ? meta.blocks.length - 1 : -1;
+                }
+
+                if (this.state.block >= 0) {
+                    // Now actually try to load the block
+                    this.LoadMessageBlock(this.state.loadedTS, this.state.block).then(() => {
+                        this.forceUpdate();
+                    });
+                    this.state.block--;
+                    if (this.state.block < 0) {
+                        date.setUTCMonth(date.getUTCMonth() - 1);
+                        this.state.loadedTS = date.valueOf();
+                    }
+                    // Exit out of the loop, job here is done
+                    break;
                 } else {
                     // Go back a month
                     date.setUTCMonth(date.getUTCMonth() - 1);
+                    this.state.loadedTS = date.valueOf();
                 }
             }
         }
+    }
+
+    // Called when the screen is opened.
+    OnOpen() {
+        this.chatId = this.props.route.params.chatId;
+        console.log("OPENED CHAT with id: " + this.chatId);
+        let key = this.GetChatDataPath();
+        this.state.began = Database.active.contains(key) ?
+            JSON.parse(Database.active.getString(key)).began : Date.now();
+
+        // Find the most recent messages
+        this.state.messages = [];
+        this.state.loadedTS = 0;
+        this.LoadMessages();
 
         // TODO indicate that messages are loading
     }
 
-    sendMessage(message) {
+    SendMessage(message) {
         // Send the last message
         this.state.messages = GiftedChat.append(this.state.messages, message);
         Peerway.SendChatMessage({
@@ -116,7 +153,7 @@ export default class Chat extends React.Component {
     }
 
     // Callback to render the input toolbar
-    renderInputToolbar(props) {
+    RenderInputToolbar(props) {
         return (<InputToolbar
             {...props}
             containerStyle={styles.inputToolbar}
@@ -124,7 +161,7 @@ export default class Chat extends React.Component {
     };
 
     // Callback to render the actions when pressed in the input toolbar
-    renderActions(props) {
+    RenderActions(props) {
         return (<Actions
             {...props}
             containerStyle={styles.actions}
@@ -136,19 +173,21 @@ export default class Chat extends React.Component {
         return (
             <View style={StyleMain.background}>
                 {/* Handles screen opening callback */}
-                <HandleEffect navigation={this.props.navigation} effect="focus" callback={() => this.onOpen()}/>
+                <HandleEffect navigation={this.props.navigation} effect="focus" callback={() => this.OnOpen()}/>
 
                 {/* Setup the chat component */}
                 <GiftedChat
                     messages={this.state.messages}
-                    renderInputToolbar={(props) => this.renderInputToolbar(props)}
-                    onSend={(message) => this.sendMessage(message)}
+                    renderInputToolbar={(props) => this.RenderInputToolbar(props)}
+                    onSend={(message) => this.SendMessage(message)}
                     user={{
                         _id: 1,
                     }}
                     alwaysShowSend
                     scrollToBottom
-                    renderActions={(props) => this.renderActions(props)}
+                    renderActions={(props) => this.RenderActions(props)}
+                    loadEarlier
+                    onLoadEarlier={() => this.LoadMessages()}
                 />
             </View>
         );
